36722e06a7ed95c5b2154657771f26396cab37fc
Mariusz Glapka <mariusz.glapka@l4a-soft.com>
2019-07-04 12:09:14 +0200

    add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+
+import Header from '../layout/Header';
 import List from './List';
 import Details from './Details';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="ui grid container app-container">
-                <div className="eight wide column app-left">
-                    <List />
+            <main>
+                <Header />
+                <div className="ui grid container app-container">
+                    <div className="eight wide column app-left">
+                        <List />
+                    </div>
+                    <div className="eight wide column app-right">
+                        <Details />
+                    </div>
                 </div>
-                <div className="eight wide column app-right">
-                    <Details />
-                </div>
-            </div>
+            </main>
         );
     }
 }
